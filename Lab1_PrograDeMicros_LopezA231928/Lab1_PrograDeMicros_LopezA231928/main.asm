64e0fb14383e87f3087329ce8836ea5bc53bbaf1
Rol0a <mlopez2018ig@gmail.com>
2026-02-06 16:07:12 -0600

    Actualizacion del PreLab 1

--- Lab1_PrograDeMicros_LopezA231928/Lab1_PrograDeMicros_LopezA231928/main.asm
+++ Lab1_PrograDeMicros_LopezA231928/Lab1_PrograDeMicros_LopezA231928/main.asm
@@ -24,36 +24,36 @@ OUT     SPH, R16
 // Configuracion MCU
 SETUP:
     LDI R16, 0x0F
-	OUT DDRB, R16
-	CBI DDRD, 2
+	OUT DDRB, R16 // Configura los pines para que sean OUTPUTS (salidas de los leds)
+	CBI DDRD, 2 //Configura los inputs, click de los botones
 	SBI PORTD, 2
-	CBI DDRD, 3
+	CBI DDRD, 3 
 	SBI PORTD, 3
 
-	LDI R20, 0x00
+	LDI R20, 0x00 //Configura la salida de cada LED
 	OUT PORTB, R20
 /****************************************/
 // Loop Infinito
 MAIN_LOOP:
-    SBIS PIND, 2
-	CALL INCREMENTO
+    SBIS PIND, 2 //Hace que checkee el registro y si esta activo manda a llamar el aumento
+	CALL SUMUP
 
-	SBIS PIND, 3
-	CALL DECREMENTO
+	SBIS PIND, 3 //Hace que checkee el registro y si esta activo manda a llamar el decremento
+	CALL DECREASE
 
 	RJMP    MAIN_LOOP
 
 /****************************************/
 // NON-Interrupt subroutines
-INCREMENTO:
-	INC R20
+SUMUP:
+	INC R20 // Va contando y saca el valor actual en el puerto B. 
 	ANDI R20, 0x0F
 	OUT PORTB, R20
 	CALL DELAY
 	RET
 
-DECREMENTO:
-	DEC R20
+DECREASE:
+	DEC R20  // Va contando y saca el valor actual en el puerto B. 
 	ANDI R20, 0x0F
 	OUT PORTB, R20
 	CALL DELAY
@@ -61,18 +61,18 @@ DECREMENTO:
 /****************************************/
 // Interrupt routines
 DELAY:
-	LDI R17, 0x20
-	LDI R18, 0xFF
-	LDI R19, 0xFF
+	LDI R11, 0x20
+	LDI R12, 0xFF
+	LDI R13, 0xFF
 
 LOOP2:
-	DEC R19
+	DEC R11
 	BRNE LOOP2
 	
-	DEC R18
+	DEC R12
 	BRNE LOOP2
 
-	DEC R17
+	DEC R13
 	BRNE LOOP2
 
 	RET
